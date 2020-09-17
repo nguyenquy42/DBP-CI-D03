ab378b8cfbecf74a54d9cdc909dcd08fd3aca365
nguyenquy42 <nguyenquy26012014@gmail.com>
2020-09-17 20:44:43 +0700

    up firt

---
+++ bai-4-day18-09-20/roman-numeral-converter/script.js
@@ -0,0 +1,18 @@
+function xuly() {
+    var n = parseInt(document.getElementById('text-input').value);
+    document.getElementById('text-output').innerHTML= ' số LA MÃ là: ' + convert(n);
+
+}
+
+var numeralCodes = [["","I","II","III","IV","V","VI","VII","VIII","IX"],         // Ones
+                    ["","X","XX","XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],   // Tens
+                    ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]];        // Hundreds
+
+function convert(num) {
+  var numeral = "";
+  var digits = num.toString().split('').reverse();
+  for (var i=0; i < digits.length; i++){
+    numeral = numeralCodes[i][parseInt(digits[i])] + numeral;
+  }
+  return numeral;  
+}
